75a593df0abbe1af0d6f412b5f73bf141fa28e8d
Amgty43 <148172960+Amgty43@users.noreply.github.com>
2023-11-28 21:40:19 -0500

    Add files via upload

---
+++ dislpay.js
@@ -0,0 +1,34 @@
+function displayCards(){
+    let card="";
+    const Table = document.getElementById("pets");
+    for(let i=0;i<petSalon.pets.lenght;i++){
+        let pet = petSalon.pets[i];
+        card+=`
+            <div class="pet">
+            <h5>Name: ${pet.nameP}</h5>
+            <p>Age: ${pet.ageP}</p>
+            <p>Gender: ${pet.genderP}</p>
+        </div>
+        `;
+    }   
+    DIV.innerHTML=card;
+}
+
+function displayTable(){
+    let row="";
+    const TABLE =document.getElementById("petTable");
+    for(let i=0;i<petSalon.pets.length;i++){
+        let pet = petSalon.pets[i];
+
+        row+=`
+            <tr id="${pet.id}">
+                <td>${pet.nameP}</td>
+                <td>${pet.AgeP}</td>
+                <td>${pet.genderP}</td>
+                <td>${pet.ColorP}</td>
+                <td> <button class="btn btn-danger btn-sm" onclick="deletePet(${pet.id});">Delete</button> </td>
+            </tr>
+        `;
+    }
+    TABLE.innerHTML=row;
+}
---
+++ register.js
@@ -0,0 +1,87 @@
+// object literal   
+let petSalon = {
+    name:"The Fashion Pet",
+    address:{
+        street:"Palm street",
+        zip: "22112",
+        state:"Baja california",
+        country:"Mexico"
+    },
+    pets:[] //this is a array
+}
+let counter = 0;
+//object constructon (special function)
+function Pet(name,age,gender,service,breed,color){
+    this.nameP=name;
+    this.agep=age;
+    this.genderp=gender;
+    this.servicep=service;
+    this.breedp=breed;
+    this.breedp=color;
+    this.id=counter++;
+}
+
+let inputName = document.getElementById("txtName");
+let inputAge = document.getElementById("txtAge");
+let inputGender = document.getElementById("txtGender");
+let inputService = document.getElementById("txtService");
+let inputBreed = document.getElementById("txtBreed");
+let inputColor = document.getElementById("txtColor");
+
+function isValid(aPet){
+    // assuming that the pet is valid
+    let validation =true;
+
+    inputName.classList.remove("error");
+    inputAge.classList.remove("error");
+
+    if(aPet.nameP==""){
+        validation=false;
+        inputName.classList.add("error");
+    }
+
+    //validate the age
+    if(aPet.ageP==""){
+        validation=false;
+        inputAge.classList.add("error")
+    }
+
+    return validation;
+}   
+
+function register(){
+    //get the values from the input
+    //use the constroucur to create an object
+let newPet = new Pet(inputName.value,inputAge.value,inputGender.value,inputService.value,inputBreed.value, inputColor.value);
+
+if(isValid(newPet)==true){
+    //push the new pet into the array
+    petSalon.pets.push(newPet);
+    //display the new pet on the console
+    // display Cards();
+    displayTable();
+    clearform();
+    }
+}
+
+function clearform(){
+    inputName.value="";
+}
+
+function deletePet(petID){
+    console.log("deleting pet" + petID);
+    // delete from the HTML
+    document.getElementById(petID).remove();
+    //remove from the array
+    petSalon.pets.splice(petSalon.pets.indexOf(petID),1);
+}
+
+function init(){
+    //creating objects using constructor
+    let scooby = new Pet("Scooby", 60, "Male", "Grooming", "Dane",);
+    let scrappy = new Pet("Scrappy", 50, "Male", "Grooming","Dane");
+    petSalon.pets.push(scooby,scrappy); //pushing the elements on th array
+    // displayCards();
+    displayTable();
+}
+window.onload=init; 
